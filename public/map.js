180b40150436e673fdbcecc57c41f8e13590bb67
Zach <zach@flaretechnologies.com.au>
2022-11-19 20:57:00 +1100

    add atmosphere

--- public/map.js
+++ public/map.js
@@ -679,3 +679,13 @@ map.on('load', function () {
     }, redrawTimeoutMs);
 });
 
+map.on('style.load', () => {
+    map.setFog({
+        color: 'rgb(186, 210, 235)', // Lower atmosphere
+        'high-color': 'rgb(36, 92, 223)', // Upper atmosphere
+        'horizon-blend': 0.02, // Atmosphere thickness (default 0.2 at low zooms)
+        'space-color': 'rgb(11, 11, 25)', // Background color
+        'star-intensity': 0.35 // Background star brightness (default 0.35 at low zoooms )
+    });
+});
+
