ce9c75b0895bda70f6bf260653b9f214755a3454
Sam Wilson <Kahn@users.noreply.github.com>
2023-06-16 18:12:42 +1000

    remove-fog

--- public/map.js
+++ public/map.js
@@ -721,13 +721,7 @@ map.on('load', function () {
 });
 
 map.on('style.load', () => {
-    map.setFog({
-        color: 'rgb(186, 210, 235)', // Lower atmosphere
-        'high-color': 'rgb(36, 92, 223)', // Upper atmosphere
-        'horizon-blend': 0.02, // Atmosphere thickness (default 0.2 at low zooms)
-        'space-color': 'rgb(11, 11, 25)', // Background color
-        'star-intensity': 0.35 // Background star brightness (default 0.35 at low zoooms )
-    });
+    map.setFog(null);
 });
 
 var mobile = findGetParameter('nav-control') || false;
